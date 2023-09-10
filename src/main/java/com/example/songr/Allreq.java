0a9e15f290b354b0ce46a204b350250bf2d250dc
anshasiq <ahmadanshasi123@hotmail.com>
2023-09-10 20:55:37 +0000

    new lab 12

--- src/main/java/com/example/songr/Allreq.java
+++ src/main/java/com/example/songr/Allreq.java
@@ -5,10 +5,7 @@ import com.example.songr.control.albums;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.List;
@@ -32,14 +29,14 @@ public class Allreq {
         m.addAttribute("name",name.toUpperCase());
         return "hello.html";
     }
-    @GetMapping("/albums")
-    public String albums(Model albmusModel){
-        Album album1 = new Album("Born to die","Lana del rey",9,180,"https://i.pinimg.com/564x/34/32/93/3432934519d074a56031263185e405a9.jpg");
-        Album album2 = new Album("The Eminem Show" , "Eminem",19 ,4260,"https://i.pinimg.com/564x/07/13/9c/07139c5c7253898b88652e97c3604466.jpg");
-        Album album3 = new Album("This Is Acting" , "Sia" , 18,4230,"https://i.pinimg.com/564x/0a/b9/3a/0ab93a940574edcb06814837533905d2.jpg");
-        albmusModel.addAttribute("albums",new Album[]{album1,album2,album3});
-        return "album.html";
-    }
+//    @GetMapping("/albums")
+//    public String albums(Model albmusModel){
+//        Album album1 = new Album("Born to die","Lana del rey",9,180,"https://i.pinimg.com/564x/34/32/93/3432934519d074a56031263185e405a9.jpg");
+//        Album album2 = new Album("The Eminem Show" , "Eminem",19 ,4260,"https://i.pinimg.com/564x/07/13/9c/07139c5c7253898b88652e97c3604466.jpg");
+//        Album album3 = new Album("This Is Acting" , "Sia" , 18,4230,"https://i.pinimg.com/564x/0a/b9/3a/0ab93a940574edcb06814837533905d2.jpg");
+//        albmusModel.addAttribute("albums",new Album[]{album1,album2,album3});
+//        return "album.html";
+//    }
     @GetMapping("")
     @ResponseBody
     public String albums(){
@@ -48,15 +45,42 @@ public class Allreq {
 
  @Autowired
     Repo Repo;
+
+//    @PutMapping("/updateAlbum")
+//    public RedirectView updateAlbumArtist (  @RequestParam String titlee,  @RequestParam String newArtist) {
+//        // Find the album by title
+//        albums album = Repo.findByTitle(titlee);
+//
+//        // Check if the album exists
+//        if (album != null) {
+//            // Update the artist
+//            album.setArtist(newArtist);
+//            Repo.save(album);
+//        }
+//
+//        // Redirect to a suitable page (e.g., details of the updated album)
+//        return new RedirectView("/");
+//    }
+
+    @DeleteMapping("/delete/{id}")
+    public RedirectView deleteAlbumById(@PathVariable Long id) {
+        // Check if the album with the given ID exists
+        if (Repo.existsById(id)) {
+            // Delete the album by ID
+            Repo.deleteById(id);
+        }
+        // Redirect to a suitable page (e.g., list of albums)
+        return new RedirectView("/");
+    }
     @PostMapping("/create")
-    public RedirectView addsong()
+    public RedirectView addsong(String title, String artist, int songCount, int length, String imageUrl )
     {
-        albums newStore = new albums("th","asd",12,32,"asd");
+        albums newStore = new albums(title, artist, songCount, length,  imageUrl);
         Repo.save(newStore);
         return new RedirectView("/");
     }
 
-    @GetMapping("/get")
+    @GetMapping("/")
     public String getSalmonCookiesStores(Model m)
     {
         List<albums> songs = Repo.findAll();
