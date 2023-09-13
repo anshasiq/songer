d53d1de75f0948701f0c4f934bcac0623d898eda
anshasiq <ahmadanshasi123@hotmail.com>
2023-09-13 14:31:37 +0000

    dd

--- readme.md
+++ readme.md
@@ -7,3 +7,11 @@ Endpoints
 - `/delete/{id}` will delete by ID .
 - `/create` it's a post-mapping will add from an html
 
+## for lab 13
+Endpoints 
+- @GetMapping("/allSongs/{albumId}") to get all song for a specific album
+- @POSTMapping(/addsong/{albumId}) it will add a song to album your standing in 
+
+
+#### click on go to display all songs belong to that album and add any songs there
+ 
--- src/main/java/com/example/songr/SongController.java
+++ src/main/java/com/example/songr/SongController.java
@@ -4,6 +4,7 @@ import com.example.songr.Repo.Repo;
 import com.example.songr.Repo.SongRepo;
 import com.example.songr.exception.AlbumNotFoundException;
 import com.example.songr.models.Song;
+//import com.example.songr.models.albums;
 import com.example.songr.models.albums;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,6 +12,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.List;
@@ -24,14 +26,14 @@ public class SongController {
     SongRepo sp;
 //  private int length_in_seconds;
 //    private int trackNumber;
-    @PostMapping("/addsong")
-    public RedirectView addsong(String title, int length_in_seconds, int trackNumber ,Long albumId){
+    @PostMapping("/addsong/{albumId}")
+    public RedirectView addsong(String title, int length_in_seconds, int trackNumber ,@PathVariable Long albumId){
         albums album= alb.findById(albumId)
                 .orElseThrow(() -> new AlbumNotFoundException("Could not find album for this song in db!"));
 //
         Song newsong= new Song(title,length_in_seconds,trackNumber,album);
         sp.save(newsong);
-        return new RedirectView("/");
+        return new RedirectView("/allSongs/{albumId}");
     }
 
 //    @GetMapping("/getsongs/{albumsId}")
@@ -44,8 +46,13 @@ public class SongController {
 //    }
 @GetMapping("/allSongs/{albumId}")
 public String getSongs(Model model, @PathVariable Long albumId) {
-    List <Song> songs =  sp.findAll();
+    System.out.println("#######^^^^####### " + albumId);
+        Optional <albums> one = alb.findById(albumId) ;
+    albums two =   one.get();
+    List <Song> songs = two.getSongs();
+//    List <Song> songs =  sp.findAll();
     model.addAttribute("song", songs);
+    model.addAttribute("albums", two);
     return "addSong.html";
 }
 
